62d75ad8d774688b288c9db538e3a515173ae49a
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-07-26 09:40:07 -0400

    added one too many resource

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,10 @@
 \c songs_dev;
 
 INSERT INTO songs (name, album, time, is_favorite) VALUES
-('Bob Marley', 'No women No cry', null, true),
-('Pink Floyd', 'The wall', null, true),
-('Post Malone', 'Austin', null, true);
+('Bob Marley', 'Legend', null, true),
+('Pink Floyd', 'Wish you were here', null, true),
+('Post Malone', 'Austin', null, true),
+('Tracy Champman', 'Fast Car', null, true),
+('Tracy Champman', 'New Beginning', null, true),
+('Bob Marley', 'Catch a Fire', null, true),
+('Pink Floyd', 'The dark side if the Moon', null, true);
